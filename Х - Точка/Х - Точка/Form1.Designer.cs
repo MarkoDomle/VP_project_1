b5c5c3791a4d658df28970e8b8385693bf79b7b2
MarkoDomle <101731007+MarkoDomle@users.noreply.github.com>
2022-07-02 21:34:41 +0200

    Add files via upload

---
+++ Х - Точка/Х - Точка/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿using System;
+
+namespace Х___Точка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.player_O_score = new System.Windows.Forms.TextBox();
+            this.player_x_score = new System.Windows.Forms.TextBox();
+            this.btn_new_game = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_new_game);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(20, 109);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1020, 603);
+            this.panel1.TabIndex = 0;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Red;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(782, 501);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(192, 78);
+            this.button12.TabIndex = 3;
+            this.button12.Text = "Излез";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(808, 6);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(193, 78);
+            this.button11.TabIndex = 2;
+            this.button11.Text = "Ресетирај резултат";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.btn8);
+            this.panel2.Controls.Add(this.btn7);
+            this.panel2.Controls.Add(this.btn6);
+            this.panel2.Controls.Add(this.btn5);
+            this.panel2.Controls.Add(this.btn4);
+            this.panel2.Controls.Add(this.btn3);
+            this.panel2.Controls.Add(this.btn2);
+            this.panel2.Controls.Add(this.btn1);
+            this.panel2.Controls.Add(this.btn0);
+            this.panel2.Location = new System.Drawing.Point(178, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(606, 440);
+            this.panel2.TabIndex = 1;
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn8.Location = new System.Drawing.Point(402, 293);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(188, 129);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn7.Location = new System.Drawing.Point(208, 293);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(188, 129);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn6.Location = new System.Drawing.Point(14, 293);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(188, 129);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn5.Location = new System.Drawing.Point(400, 158);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(188, 129);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn4.Location = new System.Drawing.Point(206, 158);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(188, 129);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn3.Location = new System.Drawing.Point(12, 158);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(188, 129);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn2.Location = new System.Drawing.Point(402, 23);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(188, 129);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn1.Location = new System.Drawing.Point(208, 23);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(188, 129);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btn0.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn0.Location = new System.Drawing.Point(14, 23);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(188, 129);
+            this.btn0.TabIndex = 0;
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 38);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Играч Х";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(465, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 38);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "vs";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(604, 56);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(212, 38);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Text = "Играч О";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "X - Точка - Игра за два играчи";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // player_O_score
+            // 
+            this.player_O_score.Enabled = false;
+            this.player_O_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player_O_score.Location = new System.Drawing.Point(828, 56);
+            this.player_O_score.Name = "player_O_score";
+            this.player_O_score.Size = new System.Drawing.Size(212, 38);
+            this.player_O_score.TabIndex = 6;
+            this.player_O_score.Text = "0";
+            this.player_O_score.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // player_x_score
+            // 
+            this.player_x_score.Enabled = false;
+            this.player_x_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player_x_score.Location = new System.Drawing.Point(230, 56);
+            this.player_x_score.Name = "player_x_score";
+            this.player_x_score.Size = new System.Drawing.Size(212, 38);
+            this.player_x_score.TabIndex = 4;
+            this.player_x_score.Text = "0";
+            this.player_x_score.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_new_game
+            // 
+            this.btn_new_game.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_new_game.Location = new System.Drawing.Point(3, 6);
+            this.btn_new_game.Name = "btn_new_game";
+            this.btn_new_game.Size = new System.Drawing.Size(156, 80);
+            this.btn_new_game.TabIndex = 4;
+            this.btn_new_game.Text = "Нова игра";
+            this.btn_new_game.UseVisualStyleBackColor = true;
+            this.btn_new_game.Click += new System.EventHandler(this.btn_new_game_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1054, 742);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.player_O_score);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.player_x_score);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Х - Точка";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox player_O_score;
+        private System.Windows.Forms.TextBox player_x_score;
+        private System.Windows.Forms.Button btn_new_game;
+    }
+}
+
---
+++ Х - Точка/Х - Точка/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Х___Точка
+{
+     public partial class Form1 : Form
+    {
+        internal Form1()
+        {
+            InitializeComponent();
+        }
+        Boolean isX = true;
+
+        void disable_when_over() { 
+            btn0.Enabled = false;
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+        }
+        void game_winner()
+        {
+            if (btn0.Text == "X" && btn1.Text == "X" && btn2.Text == "X"||
+                btn3.Text == "X" && btn4.Text == "X" && btn5.Text == "X"||
+                btn6.Text == "X" && btn7.Text == "X" && btn8.Text == "X"||
+                btn0.Text == "X" && btn4.Text == "X" && btn8.Text == "X"||
+                btn2.Text == "X" && btn4.Text == "X" && btn6.Text == "X")
+            {
+                pobeda pobeda = new pobeda();
+                pobeda.Show();
+                updateX();
+                disable_when_over();
+
+            }
+            if (btn0.Text == "O" && btn1.Text == "O" && btn2.Text == "O" ||
+               btn3.Text == "O" && btn4.Text == "O" && btn5.Text == "O" ||
+               btn6.Text == "O" && btn7.Text == "O" && btn8.Text == "O" ||
+               btn0.Text == "O" && btn4.Text == "O" && btn8.Text == "O" ||
+               btn2.Text == "O" && btn4.Text == "O" && btn6.Text == "O") {
+                pobedaZaO pobeda = new pobedaZaO();
+                pobeda.Show();
+                updateO();
+                disable_when_over();
+            }       
+        }
+        int x;
+        private void updateO()
+        {
+            x = int.Parse(player_x_score.Text);
+            x++;
+            player_x_score.Text = x.ToString();
+        }
+
+        private void updateX()
+        {
+            int x;
+            x = int.Parse(player_x_score.Text.ToString());
+            x++;
+            player_x_score.Text = Convert.ToString(x);
+        }
+
+        private void btn0_Click(object sender, EventArgs e) {
+            if (isX)
+            {
+                isX = false;
+                btn0.Text = "X";
+                btn0.Enabled = false;
+                game_winner();
+            }
+            else {
+                isX = true;
+                btn0.Text = "O";
+                btn0.Enabled = false;
+                game_winner();
+            }
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (isX)
+            {
+                isX = false;
+                btn1.Text = "X";
+                btn1.Enabled = false;
+                game_winner();
+            }
+            else
+            {
+                isX = true;
+                btn1.Text = "О";
+                btn1.Enabled = false;
+                game_winner();
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (isX)
+            {
+                isX = false;
+                btn2.Text = "X";
+                btn2.Enabled = false;
+                game_winner();
+            }
+            else
+            {
+                isX = true;
+                btn2.Text = "O";
+                btn2.Enabled = false;
+                game_winner();
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (isX)
+            {
+                isX = false;
+                btn3.Text = "X";
+                btn3.Enabled = false;
+                game_winner();
+            }
+            else
+            {
+                isX = true;
+                btn3.Text = "O";
+                btn3.Enabled = false;
+                game_winner();
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (isX)
+            {
+                isX = false;
+                btn4.Text = "X";
+                btn4.Enabled = false;
+                game_winner();
+            }
+            else
+            {
+                isX = true;
+                btn4.Text = "O";
+                btn4.Enabled = false;
+                game_winner();
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (isX)
+            {
+                isX = false;
+                btn5.Text = "X";
+                btn5.Enabled = false;
+                game_winner();
+            }
+            else
+            {
+                isX = true;
+                btn5.Text = "O";
+                btn5.Enabled = false;
+                game_winner();
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (isX)
+            {
+                isX = false;
+                btn6.Text = "X";
+                btn6.Enabled = false;
+                game_winner();
+            }
+            else
+            {
+                isX = true;
+                btn6.Text = "O";
+                btn6.Enabled = false;
+                game_winner();
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (isX)
+            {
+                isX = false;
+                btn7.Text = "X";
+                btn7.Enabled = false;
+                game_winner();
+            }
+            else
+            {
+                isX = true;
+                btn7.Text = "O";
+                btn7.Enabled = false;
+                game_winner();
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (isX)
+            {
+                isX = false;
+                btn8.Text = "X";
+                btn8.Enabled = false;
+                game_winner();
+            }
+            else
+            {
+                isX = true;
+                btn8.Text = "O";
+                btn8.Enabled = false;
+                game_winner();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            btn0.Enabled = true;
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+
+            btn0.Text = "";
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+
+            player_x_score.Text = "0";
+            player_O_score.Text = "0";
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show( "Дали сакате да излезите?", "Излез", MessageBoxButtons.OKCancel);
+            if (dr == DialogResult.OK) {
+                Application.Exit();
+            }
+        }
+
+        private void btn_new_game_Click(object sender, EventArgs e)
+        {
+            btn0.Enabled = true;
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+
+            btn0.Text = "";
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+
+        }
+    }
+}
